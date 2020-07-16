d9feab77aecaf6fa2a5173d3f44a5cf150d6d09a
aneklfae <anfekl@nael.com>
2020-07-16 18:26:46 +0900

    movie.py

--- Ch02/movie.py
+++ Ch02/movie.py
@@ -8,6 +8,7 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from datetime import datetime
+from pymongo import MongoClient as mongo
 
 #크롬 가상브라우저 실행
 chrome_options = webdriver.ChromeOptions()
@@ -32,16 +33,63 @@ review_tab = browser.find_element_by_css_selector('#movieEndTabMenu > li:nth-chi
 review_tab.click()
 browser.implicitly_wait(3)
 
-# iframe 전환
-browser.switch_to_frame('pointAfterListIframe')
+#영화제목 수집
+title = browser.find_element_by_css_selector('#content > div.article > div.mv_info_area > div.mv_info > h3 > a')
+href = title.get_attribute('href')
+i = href.rfind('=')+1
+code = href[i:]
+
+# 평점, 리뷰, 날짜 수집을 위해 현재 브라우저를 iframe 페이지로 전환
+browser.switch_to.frame('pointAfterListIframe')
 browser.implicitly_wait(3)
 
-# 평점, 리뷰, 날짜 수집
-scores  = browser.find_elements_by_css_selector('body > div > div > div.score_result > ul > li > div.star_score > em')
-reviews = browser.find_elements_by_css_selector('body > div > div > div.score_result > ul > li > div.score_reple > p > span')
-dates   = browser.find_elements_by_css_selector('body > div > div > div.score_result > ul > li > div.score_reple > dl > dt > em:nth-child(2)')
+# 최신순 클릭
+#tag_latest_a = browser.find_element_by_css_selector('#orderCheckbox > ul.sorting_list > li:nth-child(2) > a')
+#tag_latest_a.click()
+#browser.implicitly_wait(3)
 
-print(scores[9].text)
+page_num = 1
+
+while True:
+
+    # page 클릭
+    page_id = 'pagerTagAnchor'+str(page_num)
+    page_btn = browser.find_element_by_id(page_id)
+    page_btn.click()
+    browser.implicitly_wait(3)
+
+    # 평점, 리뷰, 날짜 수집
+    li_tags = browser.find_elements_by_css_selector('body > div > div > div.score_result > ul > li')
+
+    for li in li_tags:
+        score = li.find_element_by_css_selector('.star_score > em')
+        reple = li.find_element_by_css_selector('.score_reple > p > span')
+        rdate = li.find_element_by_css_selector('.score_reple > dl > dt > em:nth-child(2)')
+
+        print('score :', score.text)
+        print('reple :', reple.text)
+        print('rdate :', rdate.text)
+
+        # mongodb 접속
+        conn = mongo('mongodb://chhak:1234@192.168.100.101:27017')
+
+        # db선택
+        db = conn.get_database('chhak')
+
+        # collection 선택
+        collection = db.get_collection('movie')
+
+
+        # insert
+        collection.insert_one({'title': title.text,
+                               'code': code,
+                               'score': score.text,
+                               'reple': reple.text,
+                               'rdate': rdate.text})
+
+
+    page_num += 1
+
 
 print('종료...')
 #브라우저 종료
